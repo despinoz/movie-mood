500f4f1d9f3f2fd074e271d027245e94189686e8
Diego Espinoza <diego.espinoza@autodesk.com>
2021-02-20 08:24:14 -0500

    add qs package

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import qs from "qs";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import { useTheme } from "@material-ui/core/styles";
 import MovieDeck from "./components/MovieDeck";
@@ -8,10 +9,12 @@ const App = ({ themoviedbApiKey }) => {
   const [results, setResults] = useState([]);
 
   useEffect(() => {
+    const params = qs.stringify({
+      api_key: themoviedbApiKey,
+      sort_by: "popularity.desc",
+    });
     axios
-      .get(
-        `https://api.themoviedb.org/3/discover/movie?api_key=${themoviedbApiKey}`
-      )
+      .get(`https://api.themoviedb.org/3/discover/movie?${params}`)
       .then(({ data }) => {
         setResults(data.results);
       })
