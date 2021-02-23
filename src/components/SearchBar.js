75ba433f3fee083434b2800c0c968ca75ed75b50
Diego Espinoza <diego.espinoza@autodesk.com>
2021-02-22 23:15:40 -0500

    update SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,41 +1,26 @@
-import { useCallback, useEffect } from "react";
-import debounce from "lodash/debounce";
 import TextField from "@material-ui/core/TextField";
-import { useTheme } from "@material-ui/core/styles";
 
-const SearchBar = ({ handleOnSearch, query, setQuery, initialFetch }) => {
-  const debouncedInputChange = useCallback(debounce(handleOnSearch, 400), []);
-
-  const handleOnInputChange = (event) => {
-    setQuery(event.target.value);
-  };
-
-  useEffect(() => {
-    if (query) {
-      debouncedInputChange(query);
-    } else {
-      initialFetch();
-    }
-  }, [query, debouncedInputChange]);
-
-  return (
-    <div
-      style={{
-        width: useTheme().breakpoints.values.lg,
-        height: "400px",
-        margin: "auto",
-        textAlign: "center",
-      }}
-    >
-      <div>{query}</div>
-      <TextField
-        style={{ backgroundColor: "white", marginTop: "100px" }}
-        id="outlined-basic"
-        placeholder="Search"
-        onChange={handleOnInputChange}
-      />
-    </div>
-  );
-};
+const SearchBar = ({ handleOnInputChange }) => (
+  <div
+    style={{
+      margin: "auto",
+      textAlign: "center",
+      backgroundColor: "white",
+      position: "absolute",
+      width: "400px",
+      height: "45px",
+      borderRadius: "100px",
+      top: "15%",
+    }}
+  >
+    <TextField
+      style={{ width: "300px", marginTop: "5px" }}
+      id="outlined-basic"
+      placeholder="Search"
+      onChange={handleOnInputChange}
+      autoComplete="off"
+    />
+  </div>
+);
 
 export default SearchBar;
